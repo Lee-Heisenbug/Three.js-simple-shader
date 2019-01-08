c801f089abb0fbb5b8f58a0676d48773fd1f58a8
Lee-Heisenbug <1070068427@qq.com>
2019-01-08 21:32:03 +0800

    Move some code from index.js to scene.js that relevant to setting up the scene.

--- draw_a_box/index.js
+++ draw_a_box/index.js
@@ -13,13 +13,6 @@ var fshader = `
 
 var customMaterial, box;
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-var canvas = document.getElementById('scene-3d');
-var renderer = new THREE.WebGLRenderer({canvas});
-
-renderer.setSize( window.innerWidth, window.innerHeight );
-
 constructScene( scene );
 
 animate();
---
+++ draw_a_box/scene.js
@@ -0,0 +1,14 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var canvas = document.getElementById('scene-3d');
+var renderer = new THREE.WebGLRenderer({canvas});
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+window.addEventListener( 'resize', onWindowResize );
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
