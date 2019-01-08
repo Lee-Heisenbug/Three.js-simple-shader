1b9ad605fb0fecc7c9bc6f0c346c4b62a00d3515
李海丛 <haicong.li@goqo.com.cn>
2019-01-08 18:08:01 +0800

    Add draw_a_box example.

---
+++ draw_a_box/index.js
@@ -0,0 +1,54 @@
+var vshader = `
+    void main(){
+        gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position, 1.0 );
+    }
+`;
+
+var fshader = `
+    uniform vec3 color;
+    void main(){
+        gl_FragColor = vec4(color, 1.0);
+    }
+`;
+
+var customMaterial, box;
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var canvas = document.getElementById('scene-3d');
+var renderer = new THREE.WebGLRenderer({canvas});
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+constructScene( scene );
+
+animate();
+
+function animate() {
+    requestAnimationFrame( animate );
+    box.rotation.x += 0.1;
+    box.rotation.y += 0.1;
+	renderer.render( scene, camera );
+}
+
+function constructScene( scene ){
+    camera.position.z = 5
+
+    customMaterial = new THREE.ShaderMaterial({
+        uniforms: {
+            color: new THREE.Uniform(new THREE.Color(0xffff00))
+        },
+        vertexShader: vshader,
+        fragmentShader: fshader
+    })
+
+    box = new THREE.Mesh(
+        new THREE.BoxBufferGeometry( 1, 1, 1 ),
+        customMaterial
+    );
+    // box = new THREE.Mesh(
+    //     new THREE.BoxBufferGeometry( 1, 1, 1 ),
+    //     new THREE.MeshBasicMaterial({color:new THREE.Color(0xff0000)})
+    // );
+    scene.add(box);
+}
