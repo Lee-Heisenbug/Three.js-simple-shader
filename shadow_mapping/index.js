7671f9d39dba71a6ad68ad424de6b82e15c73762
Lee-Heisenbug <1070068427@qq.com>
2019-03-28 22:47:42 +0800

    Init shadow mapping example.

---
+++ shadow_mapping/index.js
@@ -0,0 +1,10 @@
+let shadowMappingExample = new Example( document.querySelector( '#scene' ) );
+let gui;
+
+shadowMappingExample.animate();
+
+function guiControl(){
+
+    gui = new dat.GUI();
+
+}
